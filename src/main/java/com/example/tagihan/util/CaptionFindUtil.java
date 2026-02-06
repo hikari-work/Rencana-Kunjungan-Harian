18ba5e4563c59bee8b6f546e884ea7059f3c7d29
piyan <vianstefani754@gmail.com>
2026-02-06 08:09:14 +0700

    Fix : NullPointerException

--- src/main/java/com/example/tagihan/util/CaptionFindUtil.java
+++ src/main/java/com/example/tagihan/util/CaptionFindUtil.java
@@ -5,18 +5,6 @@ import com.example.tagihan.dto.WebhookPayload;
 public class CaptionFindUtil {
 
     public static String caption(WebhookPayload payload) {
-        if (payload == null || payload.getPayload() == null) {
-            return null;
-        }
-
-        if (payload.getPayload().isText()) {
-            return payload.getPayload().getBody();
-        }
-
-        if (payload.getPayload().getImage() != null) {
-            return payload.getPayload().getImage().getCaption();
-        }
-
-        return null;
+        return payload.getPayload().isText() ? payload.getPayload().getBody() : payload.getPayload().getImage().getCaption();
     }
 }
